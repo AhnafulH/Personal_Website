3f08a00371620bc2745de68557508911361c95d5
Ahnaful <hoque.ahnaful@gmail.com>
2024-05-25 17:28:51 -0600

    Added Navigation bar

--- portofolio-website/app/page.tsx
+++ portofolio-website/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div class="container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col">
+      <div className="container mx-auto px-12 py-4">
+        <Header/>
         <HeroSection/>
       </div>
     </main>
   );
 }
+
+//bg-[#121212]
--- portofolio-website/components/HeroSection.jsx
+++ portofolio-website/components/HeroSection.jsx
@@ -7,7 +7,7 @@ import { TypeAnimation } from 'react-type-animation';
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="grid grid-cols-1 sm:grid-cols-12 py-20">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"> 
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I am{" "}</span>
@@ -22,11 +22,13 @@ const HeroSection = () => {
                 'Software Engineer',
                 1000,
                 'Student',
+                1000,
+                'Ahnaful',
                 1000
               ]}
               wrapper="span"
-              speed={25}
-              repeat={Infinity}
+              speed={50}
+              repeat={0}
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
