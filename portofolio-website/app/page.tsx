aee21121abab7d739a7e2e61d54110499a7cd9ee
Ahnaful <hoque.ahnaful@gmail.com>
2024-05-30 20:02:58 -0600

    Added Aurora Background

--- portofolio-website/app/page.tsx
+++ portofolio-website/app/page.tsx
@@ -1,17 +1,19 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import AuroraBackground from "@/components/AuroraBackground";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 import ScrollBtn from "@/components/ScrollBtn";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="container mx-auto px-12 py-4">
-        <Header/>
-        <HeroSection/>
-        {/* <ScrollBtn/> */}
-      </div>
-    </main>
+    <AuroraBackground>
+      <main className="flex min-h-screen flex-col">
+        <div className="container mx-auto px-12 py-4">
+          <Header/>
+          <HeroSection/>
+          {/* <ScrollBtn/> */}
+        </div>
+      </main>
+      </AuroraBackground>
   );
 }
 
