3e428b531e780b480926a0d44b2687d1f17494a9
Ahnaful <hoque.ahnaful@gmail.com>
2024-05-31 20:20:02 -0600

    Started About section. Added necessary files

--- portofolio-website/app/page.tsx
+++ portofolio-website/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import { AuroraBackground } from "@/components/ui/aurora-background";
-import ScrollBtn from "@/components/ScrollBtn";
+import About from "@/components/About";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="container mx-auto px-12 py-4">
           <Header/>
           <HeroSection/>
-          {/* <ScrollBtn/> */}
+          <About/>
         </div>
       </main>
       </AuroraBackground>
---
+++ portofolio-website/components/About.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section>
+        <h2 className="text-3xl font-medium">
+            About Me
+        </h2>
+
+        <p>
+
+        </p>
+
+        <p>
+
+        </p>
+    </section>
+  )
+}
+
+export default About
