c222ac7daff0738d86ee7fdb589abf5b04df86e8
Ahnaful <hoque.ahnaful@gmail.com>
2024-06-02 20:51:50 -0600

    Worked on experience section

--- portofolio-website/app/page.tsx
+++ portofolio-website/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
           <Header/>
           <HeroSection/>
           <About/>
+          <Experience/>
         </div>
       </main>
       </AuroraBackground>
