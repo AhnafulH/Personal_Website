3ecbbd43bc0c4357f197d85a142b8df829339cd3
Ahnaful <hoque.ahnaful@gmail.com>
2024-06-03 20:57:50 -0600

    Commit after fixing git. Hopefully this works

---
+++ portofolio-website/lib/experience.ts
@@ -0,0 +1,10 @@
+const experiences = [
+    {
+        title: " Functional Safety Software Development Student",
+        compay: "BlackBerry QNX",
+        date: "January 2024 - August 2024",
+        points: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+];
+
+export { experiences };
