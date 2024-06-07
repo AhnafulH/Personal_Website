fc656cd86b61529de400e2a1f812147432f736de
Ahnaful <hoque.ahnaful@gmail.com>
2024-06-06 20:18:07 -0600

    Edited experience.tsx and added new animation at motion.js

--- portofolio-website/components/Experience.tsx
+++ portofolio-website/components/Experience.tsx
@@ -3,11 +3,21 @@ import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timel
 import 'react-vertical-timeline-component/style.min.css';
 import { motion } from "framer-motion";
 
+import { textVariant } from "@/utils/motion";
+
 const Experience = () => {
   return (
-    <VerticalTimeline>
+    <motion.div variants={textVariant()}>
+      <p>
+        What I have done so far
+      </p>
+      <p>
+        Experience
+      </p>
+      <VerticalTimeline>
       
-    </VerticalTimeline>
+      </VerticalTimeline>
+    </motion.div>
   )
 }
 
