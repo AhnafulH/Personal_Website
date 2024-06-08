be594916110738552b93233396e07b0e16095214
Ahnaful <hoque.ahnaful@gmail.com>
2024-06-08 14:46:31 -0600

    Fixed error in Experience.tsx with use client

--- portofolio-website/components/Experience.tsx
+++ portofolio-website/components/Experience.tsx
@@ -1,30 +1,54 @@
+"use client"
+
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { motion } from 'framer-motion';
 
-const textVariant = () => ({
-  hidden: { opacity: 0, y: -20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.5,
-      ease: 'easeInOut',
+import { experiences } from '@/lib/experiences';
+const textVariant = {
+    hidden: {
+      y: -50,
+      opacity: 0,
     },
-  },
-});
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: 0.5,
+      },
+    },
+};
 
-const Experience: React.FC = () => {
+const ExperienceCard = ({ experience }) => {
+  <VerticalTimelineElement>
+    
+  </VerticalTimelineElement>
+}
+
+
+const Experience = () => {
+
   return (
-    <div className="relative z-10 p-8">
+    <section>
+      <motion.div variants={textVariant} className="relative z-10 p-8">
         <h2 className="text-2xl font-bold mb-4">
           What I have done so far
         </h2>
         <p className="text-lg mb-8">
           Experience
         </p>
+      </motion.div>
+      <div>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience}/>
+          ))}
+        </VerticalTimeline>
       </div>
+    </section>
   );
 };
 
