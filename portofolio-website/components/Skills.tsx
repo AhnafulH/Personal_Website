ea74d2406214918005d5a1dfc3ed71f5ce84c966
Ahnaful <hoque.ahnaful@gmail.com>
2024-06-20 19:22:16 -0600

    Fixed image error. Image now showing

--- portofolio-website/components/Skills.tsx
+++ portofolio-website/components/Skills.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { skill } from '@/lib/skill'
+import Image from 'next/image'
 
 import blackberryqnx_logo from '@/public/images/blackberryqnx_logo.png'
-
+import C from '@/public/skills_logo/c.svg'
 
 const Skills = () => {
   return (
@@ -10,7 +11,10 @@ const Skills = () => {
       {skill.map((item, index) => (
         <div key={index}>
            <div className="w-16 h-16 mb-4">
-            <img src={blackberryqnx_logo.src} alt={item.skill_name} />
+            {/* <imgage src={c} alt={item.skill_name} /> */}
+            {/* <CIcon alt={item.skill_name} className="w-full h-full" /> */}
+            <Image src={C} alt={item.skill_name} width={500}
+      height={500} ></Image>
             </div>
         </div>
     ))}
