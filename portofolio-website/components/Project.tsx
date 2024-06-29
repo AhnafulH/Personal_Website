7170d94b56262b859167ebfc3a7f31e3be100009
Ahnaful <hoque.ahnaful@gmail.com>
2024-06-28 20:07:34 -0600

    Added Project header and started coding project container element

--- portofolio-website/components/Project.tsx
+++ portofolio-website/components/Project.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <section className="py-20">
+      <div className="text-center mb-10">
+        <h2 className="font-bold text-4xl md:text-5xl text-center">Projects</h2>
+        <p className="text-lg text-gray-600 py-5">A small selection of recent projects</p>
+      </div>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+
+      </div>
+    </section>
   )
 }
 
