7c462fb6f64039a658fe4a745988d81955c551ae
Ahnaful <hoque.ahnaful@gmail.com>
2024-07-07 12:00:44 -0600

    Worked on project card

--- portofolio-website/components/Project2.tsx
+++ portofolio-website/components/Project2.tsx
@@ -7,51 +7,53 @@ import Link from "next/link";
 
 const Project2 = () => {
   return (
-    <CardContainer className="inter-var">
-    <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-      <CardItem
-        translateZ="50"
-        className="text-xl font-bold text-neutral-600 dark:text-white"
-      >
-        Make things float in air
-      </CardItem>
-      <CardItem
-        as="p"
-        translateZ="60"
-        className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-      >
-        Hover over this card to unleash the power of CSS perspective
-      </CardItem>
-      <CardItem translateZ="100" className="w-full mt-4">
-        <Image
-          src="/images/p4.svg"
-          height="1000"
-          width="1000"
-          className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-          alt="thumbnail"
-        />
-      </CardItem>
-      <div className="flex justify-between items-center mt-20">
-        <CardItem
-          translateZ={20}
-          as={Link}
-          href="https://twitter.com/mannupaaji"
-          target="__blank"
-          className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+    <section>
+      <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem 
+          translateZ="100" className="w-full mt-4"
+          // rotateX={20}
+          // rotateY={10}
+          // rotateZ={10}
         >
-          Try now →
+          <Image
+            src="/images/smart_traffic_control.png"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
         </CardItem>
         <CardItem
-          translateZ={20}
-          translateX={40}
-          as="button"
-          className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          translateZ="50"
+          as="h1"
+          className="lg:text-2xl md:text-xl text-base line-clamp-1 font-bold text-neutral-600 dark:text-white mt-4"
         >
-          Sign up
+          Make things float in air
         </CardItem>
-      </div>
-    </CardBody>
-  </CardContainer>
+        <CardItem
+          translateZ={40}
+        >
+        <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "black",
+                  margin: "1vh 0",
+                }}
+              >
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              </p>
+        </CardItem>
+      </CardBody>
+
+      
+    </CardContainer>
+
+  </section>
+
+  
+
+  
   )
 }
 
