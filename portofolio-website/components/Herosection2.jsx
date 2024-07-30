c2f5e24497b6e82ea6a2b6c025680b73c0b9b63f
Ahnaful <hoque.ahnaful@gmail.com>
2024-07-29 19:52:03 -0600

    Added new subheader to hero section

--- portofolio-website/components/Herosection2.jsx
+++ portofolio-website/components/Herosection2.jsx
@@ -9,7 +9,6 @@ const HeroSection2 = () => {
           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <div className="my-4">
               <h1 className="text-center text-[40px] md:text-6xl lg:text-7xl font-bold leading-snug tracking-wider">
-              {/* Transforming Concepts into Seamless User Experiences */}
               Coding the Future One Project at a Time
               </h1>
             </div>
@@ -18,8 +17,20 @@ const HeroSection2 = () => {
               initial={{ opacity: 0, y: -50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8 }}>
-                            Hey there! I'm Ahnaful
+                Hey there! I'm Ahnaful
             </motion.h2>
+            {/* <motion.h3 
+                            className="text-center text-xl md:text-2xl lg:text-3xl font-light leading-snug tracking-wider mt-4"
+                            initial={{ opacity: 0, y: -20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.8 }}
+                        >
+                            Student | Developer | Engineer from Edmonton, Canada
+                        </motion.h3> */}
+             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl my-3">
+            {/* Hi! I&apos;m Ahnaful, Computer Engineering Student based in Edmonton */}
+            Student | Developer | Engineer from Edmonton, Canada
+          </p>
           </div>
         </div>
       </section>
